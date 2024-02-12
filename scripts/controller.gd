056951c5e779bf1ba191633ed1ff001ae4eba2e5
Paladin  Richards <patrickrich07@gmail.com>
2024-02-12 08:53:35 -0600

    Basic Jump animation
    A basic head bob animation was added, some code was cleaned up.

--- scripts/controller.gd
+++ scripts/controller.gd
@@ -6,12 +6,14 @@ extends CharacterBody3D
 @onready var crouchingBody = $Crouching_Body
 @onready var checkHead = $Check_Head
 @onready var neck = $Neck
-@onready var camera3d = $Neck/Head/Camera3D
+@onready var camer3d = $Neck/Head/Eyes/Camera3D
+@onready var eyes = $Neck/Head/Eyes
+@onready var animationPlayer = $Neck/Head/Eyes/AnimationPlayer
 
 
 # Speed vars
 var currentSpeed = 5.0
-@export var wankingSpeed = 5.0
+@export var walkingSpeed = 5.0
 @export var sprintingSpeed = 9.0
 @export var crouchingSpeed = 3.0
 
@@ -33,6 +35,19 @@ var slideTimer = 0.0
 var slideTimerMax = 1.0
 var slideVector = Vector2.ZERO
 var slideSpeed = 10.0
+
+# Head bobbing var
+const sprintingBobSpeed = 22.0
+const walkingBobSpeed = 14.0
+const crouchingBobSpeed = 10.0
+
+const sprintBobInt = 0.2
+const walkingBobInt = 0.1
+const crouchingBobInt = 0.05
+
+var bobVector = Vector2.ZERO
+var bobIndex = 0.0
+var currentBobInt = 0.0
 
 # Input vars
 var direction = Vector3.ZERO
@@ -61,7 +76,7 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("Left", "Right", "Forward", "Backward")
 	# Check if crouching (Crouching is more important to check then sprint)
 	if Input.is_action_pressed("Crouch") || sliding:
-		currentSpeed = crouchingSpeed
+		currentSpeed = lerp(currentSpeed, crouchingSpeed, delta * lerpSpeed)
 		head.position.y = lerp(head.position.y,crouchDepth, delta * lerpSpeed)
 		#Change Collider so we can fit whole body
 		standingBody.disabled = true
@@ -86,12 +101,12 @@ func _physics_process(delta):
 		head.position.y = lerp(head.position.y, 0.0, delta * lerpSpeed)
 		#sprint
 		if Input.is_action_pressed("Sprint"):
-			currentSpeed = sprintingSpeed
+			currentSpeed = lerp(currentSpeed, sprintingSpeed, delta * lerpSpeed)
 			walking = false
 			sprinting = true
 			crouching = false
 		else:
-			currentSpeed = wankingSpeed
+			currentSpeed = lerp(currentSpeed, walkingSpeed, delta * lerpSpeed)
 			walking = true
 			sprinting = false
 			crouching = false
@@ -100,13 +115,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("Free_Look") || sliding:
 		freeLook = true
 		if sliding:
-			camera3d.rotation.z = lerp(camera3d.rotation.z, deg_to_rad(7.0), delta * lerpSpeed)
+			eyes.rotation.z = lerp(eyes.rotation.z, deg_to_rad(7.0), delta * lerpSpeed)
 		else:
-			camera3d.rotation.z = deg_to_rad(neck.rotation.y * freeLookTilt)
+			eyes.rotation.z = deg_to_rad(neck.rotation.y * freeLookTilt)
 	else:
 		freeLook = false
 		neck.rotation.y = lerp(neck.rotation.y,0.0, delta * lerpSpeed)
-		camera3d.rotation.z = lerp(camera3d.rotation.z,0.0, delta * lerpSpeed)
+		eyes.rotation.z = lerp(eyes.rotation.z,0.0, delta * lerpSpeed)
 	
 	# Handle sliding
 	if sliding:
@@ -115,7 +130,27 @@ func _physics_process(delta):
 			sliding = false
 			freeLook = false
 			print("Sliding done!")
+			
+	# Handle Bobbing
+	if sprinting:
+		currentBobInt = sprintBobInt
+		bobIndex += sprintingBobSpeed * delta
+	elif walking:
+		currentBobInt = walkingBobInt
+		bobIndex += walkingBobSpeed * delta
+	elif crouching:
+		currentBobInt = crouchingBobInt
+		bobIndex += crouchingBobSpeed * delta
 	
+	if is_on_floor() && !sliding && input_dir != Vector2.ZERO:
+		bobVector.y = sin(bobIndex)
+		bobVector.x = sin(bobIndex/2)+0.5
+		eyes.position.y = lerp(eyes.position.y, bobVector.y * (currentBobInt / 2.0), delta * lerpSpeed )
+		eyes.position.x = lerp(eyes.position.x, bobVector.x * currentBobInt, delta * lerpSpeed )
+	else:
+		eyes.position.y = lerp(eyes.position.y, 0.0, delta * lerpSpeed )
+		eyes.position.x = lerp(eyes.position.x, 0.0, delta * lerpSpeed )
+		
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -124,21 +159,19 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jumpVelocity
 		sliding = false
+		animationPlayer.play("jump")
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*lerpSpeed)
+	if is_on_floor():
+		direction = lerp(direction,(transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta*lerpSpeed)
 	
 	if sliding:
 		direction = (transform.basis * Vector3(slideVector.x, 0.0, slideVector.y)).normalized()
+		currentSpeed = (slideTimer+0.1) * slideSpeed
 	
 	if direction:
 		velocity.x = direction.x * currentSpeed
 		velocity.z = direction.z * currentSpeed
-		if sliding:
-			velocity.x = direction.x * (slideTimer+0.1) * slideSpeed
-			velocity.z = direction.z * (slideTimer+0.1) * slideSpeed
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, currentSpeed)
 		velocity.z = move_toward(velocity.z, 0, currentSpeed)
